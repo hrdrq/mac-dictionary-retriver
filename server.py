ac37cdad0138da329d37caaa5e7c1c22db01c2bb
hrdrq <son.munehiro@gmail.com>
2024-09-23 18:14:26 +0900

    add proxy feature for docker container

--- server.py
+++ server.py
@@ -4,12 +4,17 @@ import sys
 from subprocess import PIPE
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs, urlparse
+import os
 
 address = ('localhost', 8768)
 
 class RetriverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         query = parse_qs(urlparse(self.path).query)
+        if query.get('open_audio_file', ['false'])[0] == 'true':
+            self.open_audio_file(query.get('path')[0], query.get('file_name')[0])
+            return
+            
         word, dictionary = query.get('word'), query.get('dictionary')
         if not word or not dictionary:
             self.send_response(400)
@@ -23,6 +28,10 @@ class RetriverHandler(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'text/html; charset=utf-8')
         self.end_headers()
         self.wfile.write(res.encode())
+        
+    def open_audio_file(self, path, file_name):
+        print(f'open_audio_file: {path}/{file_name}')
+        os.system(f'open -a /Applications/Audacity.app {path}/{file_name}')
 
 with HTTPServer(address, RetriverHandler) as server:
     server.serve_forever()
